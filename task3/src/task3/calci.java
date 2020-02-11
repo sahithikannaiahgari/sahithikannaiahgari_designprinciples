81f4de6a9763d2f07bf3064240ec16522f85e46c
sahithikannaiahgari <59208403+sahithikannaiahgari@users.noreply.github.com>
2020-02-11 23:43:32 +0530

    Add files via upload

---
+++ task3/src/task3/calci.java
@@ -0,0 +1,10 @@
+package task3;
+
+public class calci {
+	int add(int a,int b) {return a+b;}
+	int sub(int a,int b) {return a-b;}
+	int mul(int a,int b) {return a*b;}
+	float div(int a,int b) {return (float)a/(float) b;}
+
+
+}
